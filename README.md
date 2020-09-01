7b1d9716cb503c9ee61e423c1aa0d265f3e505be
Howard.Zuo <leftstick@qq.com>
2020-09-01 16:28:00 +0800

    feat: add .less detect feature

--- README.md
+++ README.md
@@ -1,42 +1,97 @@
-<!-- @format -->
+# umi-plugin-dynamic-antd-theme
 
-# umi-plugin-antd-theme
+[![NPM Version][npm-image]][npm-url]
+[![Build Status][travis-image]][travis-url]
+![][david-url]
+![][dt-url]
+[![code style: prettier][prettier-image]][prettier-url]
+![][license-url]
 
-[![NPM version](https://img.shields.io/npm/v/umi-plugin-antd-theme.svg?style=flat)](https://npmjs.org/package/umi-plugin-antd-theme) [![NPM downloads](http://img.shields.io/npm/dm/umi-plugin-antd-theme.svg?style=flat)](https://npmjs.org/package/umi-plugin-antd-theme)
+With `umi@3`, Just install `umi-plugin-dynamic-antd-theme`, it shall be loaded by `umi` automatically.
 
 ## Usage
 
-Configure in `config/theme.config.json`,
+### Theme configuration
+
+You should not use `.umirc.ts`, but with `config/config.ts` instead.
+
+And add theme configure file `config/theme.config.json` with below content:
 
 ```json
 {
   "theme": [
     {
-      "theme": "dark",
-      "fileName": "dark.css"
+      "theme": "dark", // theme key
+      "fileName": "dark.css", // theme file name, will be generated as static assets
+      "modifyVars": {
+        // variables that will be replaced in antd
+        "@primary-color": "#F5222D"
+      }
     },
     {
+      "theme": "mingQing",
       "fileName": "mingQing.css",
       "modifyVars": {
         "@primary-color": "#13C2C2"
       }
     }
   ],
-  // 是否压缩css
+  // minify css
   "min": true,
   // css module
   "isModule": true,
-  // 忽略 antd 的依赖
+  // ignore antd dependency
   "ignoreAntd": false,
-  // 忽略 pro-layout
+  // ignore pro-layout dependency
   "ignoreProLayout": false,
-  // 不使用缓存
-  "cache": true
+  // no cache
+  "cache": false
 }
 ```
 
-you can get config in `window.umi_plugin_ant_themeVar`
+You can use this config via `window.umi_plugin_ant_themeVar`.
+
+### App-level theme variables
+
+Define app-level theme variables(with prefix `--`) in `global.less`, for example:
+
+```less
+.body-wrap-theme1 {
+  --font-color: #000000;
+  --bg-color: #011313;
+}
+
+.body-wrap-theme2 {
+  --font-color: #ffffff;
+  --bg-color: #ffffff;
+}
+```
+
+> `theme1`, `theme2` is the theme key we mentioned in [configuration](#theme-configuration)
+
+Use app-level theme variables(with `var` function) in your component `.less` file, for example:
+
+```less
+.flatButton {
+  color: var(--font-color);
+  background: var(--bg-color);
+}
+```
+
+### Switch theme dynamically
+
+Please refer to [ThemeSwitch.tsx](https://github.com/DFocusGroup/generator-umi/blob/master/generators/app/templates/src/components/buttons/ThemeSwitch/index.tsx)
 
 ## LICENSE
 
-MIT
+[MIT License](https://raw.githubusercontent.com/leftstick/umi-plugin-dynamic-antd-theme/master/LICENSE)
+
+[npm-url]: https://npmjs.org/package/umi-plugin-dynamic-antd-theme
+[npm-image]: https://badge.fury.io/js/umi-plugin-dynamic-antd-theme.png
+[david-url]: https://david-dm.org/leftstick/umi-plugin-dynamic-antd-theme.png
+[travis-image]: https://www.travis-ci.org/leftstick/umi-plugin-dynamic-antd-theme.svg?branch=master
+[travis-url]: https://travis-ci.com/leftstick/umi-plugin-dynamic-antd-theme
+[dt-url]: https://img.shields.io/npm/dt/umi-plugin-dynamic-antd-theme.svg
+[license-url]: https://img.shields.io/github/license/leftstick/umi-plugin-dynamic-antd-theme
+[prettier-image]: https://img.shields.io/badge/code_style-prettier-ff69b4.svg
+[prettier-url]: https://github.com/prettier/prettier
--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,97 @@
 /** @format */
 
 // - https://umijs.org/plugin/develop.html
-import { IApi } from 'umi';
-import { join } from 'path';
-import serveStatic from 'serve-static';
-import rimraf from 'rimraf';
-import { existsSync, mkdirSync } from 'fs';
-import defaultTheme from './defaultTheme';
+import { IApi } from 'umi'
+import { join } from 'path'
+import serveStatic from 'serve-static'
+import rimraf from 'rimraf'
+import { existsSync, mkdirSync } from 'fs'
+import defaultTheme from './defaultTheme'
 
-const buildCss = require('antd-pro-merge-less');
-const winPath = require('slash2');
+const buildCss = require('antd-pro-merge-less')
+const winPath = require('slash2')
 
 interface themeConfig {
-  theme?: string;
-  fileName: string;
-  key: string;
-  modifyVars?: { [key: string]: string };
+  theme?: string
+  fileName: string
+  key: string
+  modifyVars?: { [key: string]: string }
 }
 
-export default function (api: IApi) {
-  api.modifyDefaultConfig((config) => {
+export default function(api: IApi) {
+  api.modifyDefaultConfig(config => {
     config.cssLoader = {
       modules: {
         getLocalIdent: (
           context: {
-            resourcePath: string;
+            resourcePath: string
           },
           _: string,
-          localName: string,
+          localName: string
         ) => {
           if (
             context.resourcePath.includes('node_modules') ||
             context.resourcePath.includes('ant.design.pro.less') ||
             context.resourcePath.includes('global.less')
           ) {
-            return localName;
+            return localName
           }
-          const match = context.resourcePath.match(/src(.*)/);
+          const match = context.resourcePath.match(/src(.*)/)
           if (match && match[1]) {
-            const antdProPath = match[1].replace('.less', '');
+            const antdProPath = match[1].replace('.less', '')
             const arr = winPath(antdProPath)
               .split('/')
               .map((a: string) => a.replace(/([A-Z])/g, '-$1'))
-              .map((a: string) => a.toLowerCase());
-            return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+              .map((a: string) => a.toLowerCase())
+            return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-')
           }
-          return localName;
-        },
-      },
-    };
-    return config;
-  });
+          return localName
+        }
+      }
+    }
+    return config
+  })
   // 给一个默认的配置
   let options: {
-    theme: themeConfig[];
-    min?: boolean;
-  } = defaultTheme;
+    theme: themeConfig[]
+    min?: boolean
+  } = defaultTheme
 
   // 从固定的路径去读取配置，而不是从 config 中读取
-  const themeConfigPath = winPath(join(api.paths.cwd, 'config/theme.config.json'));
+  const themeConfigPath = winPath(join(api.paths.cwd, 'config/theme.config.json'))
   if (existsSync(themeConfigPath)) {
-    options = require(themeConfigPath);
+    options = require(themeConfigPath)
   }
-  const { cwd, absOutputPath, absNodeModulesPath } = api.paths;
-  const outputPath = absOutputPath;
-  const themeTemp = winPath(join(absNodeModulesPath, '.plugin-theme'));
+  const { cwd, absOutputPath, absNodeModulesPath } = api.paths
+  const outputPath = absOutputPath
+  const themeTemp = winPath(join(absNodeModulesPath, '.plugin-theme'))
+  let needBuildCss = false,
+    isFirstCompileFinished = true
 
   // 增加中间件
   api.addMiddewares(() => {
-    return serveStatic(themeTemp);
-  });
+    return serveStatic(themeTemp)
+  })
 
   // 增加一个对象，用于 layout 的配合
   api.addHTMLHeadScripts(() => [
     {
-      content: `window.umi_plugin_ant_themeVar = ${JSON.stringify(options.theme)}`,
-    },
-  ]);
+      content: `window.umi_plugin_ant_themeVar = ${JSON.stringify(options.theme)}`
+    }
+  ])
 
   // 编译完成之后
   api.onBuildComplete(({ err }) => {
     if (err) {
-      return;
+      return
     }
-    api.logger.info('💄  build theme');
+    api.logger.info('💄  build theme')
 
     try {
       if (existsSync(winPath(join(outputPath, 'theme')))) {
-        rimraf.sync(winPath(join(outputPath, 'theme')));
+        rimraf.sync(winPath(join(outputPath, 'theme')))
       }
-      mkdirSync(winPath(join(outputPath, 'theme')));
+      mkdirSync(winPath(join(outputPath, 'theme')))
     } catch (error) {
       // console.log(error);
     }
@@ -97,59 +99,84 @@ export default function (api: IApi) {
     buildCss(
       cwd,
       options.theme.map(
-        (theme) => ({
+        theme => ({
           ...theme,
-          fileName: winPath(join(outputPath, 'theme', theme.fileName)),
+          fileName: winPath(join(outputPath, 'theme', theme.fileName))
         }),
         {
           min: true,
-          ...options,
-        },
-      ),
+          ...options
+        }
+      )
     )
       .then(() => {
-        api.logger.log('🎊  build theme success');
+        api.logger.log('🎊  build theme success')
       })
-      .catch((e) => {
-        console.log(e);
-      });
-  });
+      .catch(e => {
+        console.log(e)
+      })
+  })
+
+  class WebpackPluginForUmiPluginAntdTheme {
+    apply(compiler: any) {
+      compiler.hooks.watchRun.tapAsync('WebpackPluginForUmiPluginAntdTheme', (_compiler: any, done: any) => {
+        const watchFileSystem = (_compiler as any).watchFileSystem
+        const watcher = watchFileSystem.watcher || watchFileSystem.wfs.watcher
+        const lessFiles = Object.keys(watcher.mtimes).filter(fileName => /\.less\b/.test(fileName))
+        needBuildCss = isFirstCompileFinished || lessFiles.length > 0
+        done()
+      })
+    }
+  }
+
+  api.chainWebpack({
+    fn(initialValue) {
+      initialValue.plugin('WebpackPluginForUmiPluginAntdTheme').use(WebpackPluginForUmiPluginAntdTheme)
+      return initialValue
+    }
+  })
 
   // dev 之后
   api.onDevCompileDone(() => {
-    api.logger.info('cache in :' + themeTemp);
-    api.logger.info('💄  build theme');
+    isFirstCompileFinished = false
+    if (!needBuildCss) {
+      api.logger.info('skip merge-less since no less variable changed')
+      return
+    }
+
+    api.logger.info('cache in :' + themeTemp)
+    api.logger.info('💄  build theme')
     // 建立相关的临时文件夹
     try {
       if (existsSync(themeTemp)) {
-        rimraf.sync(themeTemp);
+        rimraf.sync(themeTemp)
       }
       if (existsSync(winPath(join(themeTemp, 'theme')))) {
-        rimraf.sync(winPath(join(themeTemp, 'theme')));
+        rimraf.sync(winPath(join(themeTemp, 'theme')))
       }
 
-      mkdirSync(themeTemp);
+      mkdirSync(themeTemp)
 
-      mkdirSync(winPath(join(themeTemp, 'theme')));
+      mkdirSync(winPath(join(themeTemp, 'theme')))
     } catch (error) {
       // console.log(error);
     }
 
     buildCss(
       cwd,
-      options.theme.map((theme) => ({
+      options.theme.map(theme => ({
         ...theme,
-        fileName: winPath(join(themeTemp, 'theme', theme.fileName)),
+        fileName: winPath(join(themeTemp, 'theme', theme.fileName))
       })),
       {
-        ...options,
-      },
+        ...options
+      }
     )
       .then(() => {
-        api.logger.log('🎊  build theme success');
+        api.logger.log('🎊  build theme success')
       })
-      .catch((e) => {
-        console.log(e);
-      });
-  });
+      .catch(e => {
+        console.log(e)
+      })
+  })
 }
